d94a192e1971590ef81303b5c2c49b8e0692c77c
Moha <mohamedouikssani@gmail.com>
2022-05-06 00:33:37 +0200

    buscar tesoro mas cerca con bfs

--- game/AICuseta.cc
+++ game/AICuseta.cc
@@ -27,27 +27,18 @@ struct PLAYER_NAME : public Player {
   bool unidad_mia(int unit_id)      
   {
     bool ret=false;
+    for(unsigned int i=0; i<  wizards(me()).size() and ret==false; ++i)
+    {
+      if(unit_id ==  wizards(me())[i]) ret=true;
+    }
     for(unsigned int i=0; i<  dwarves(me()).size() and ret==false; ++i)
     {
-      if(unit_id ==  my_units(me())[i]) ret=true;
+      if(unit_id ==  dwarves(me())[i]) ret=true;
     }
     return ret;
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  //tesoro mas cerca
   vector<Pos> pos_tesoros;
   Pos tesoro_mas_cerca(Unit &u)
   {
@@ -81,32 +72,266 @@ struct PLAYER_NAME : public Player {
     return mas_cerca;
   }
 
-  //Entrar dentro
-  void entrar_dentro(Unit &u)
-  {
-    if(u.pos.i== 0 or 2) command(u.id,Bottom);
-    else if(u.pos.i== 58 or 59) command(u.id,Top);
 
-    else if(u.pos.j== 0 or 1) command(u.id,Right);
-    else if(u.pos.j== 58 or 59) command(u.id,Left);
+
+  //Entrar dentro
+  void entrar_dentro(Unit &u)   //añadir control unidades enemigas
+  {
+    if(u.pos.i==0 or u.pos.i==1) command(u.id,Bottom);
+    else if(u.pos.i==59 or u.pos.i==58) command(u.id,Top);
+
+    else if(u.pos.j== 0 or u.pos.j== 1) command(u.id,Right);
+    else command(u.id,Left);
   }
+
+
+
 
   //Si hay Balrog cerca
-  bool balrog_cerca()
+  bool balrog_cerca(Unit &u) //DEVOLVER LA POSICION DEL BALROG
+  {
+    bool res=false;
+    for(int i=0; i<4; ++i)
+    {
+      int d=0;
+      Pos pos_balrog = unit(balrog_id()).pos;
+      if( pos_balrog == (u.pos+ Dir(d))+Dir(d) ) res=true;
+      d+=2;
+    }
+    return res;
+  }
+
+  //Buscar el duende mas cerca con menos vida
+  void curar_dwarv()
   {
 
   }
 
 
 
+  const int INF = 1e9;
+  vector<pair<int,int>> dirs = { {0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1} };
 
-  //Buscar al mago mas cerca si duende con poca vida 
+  bool bon_vei (const vector<vector<Cell>>& M, uint i, uint j) {
+    return i >= 0 and j >= 0 and i < M.size() and j < M[0].size() and M[i][j].type != Granite; //!GRANITE
+  }
 
-  //Buscar al duende mas cerca y con mas daño
+  void bfs_tresor (vector<vector<Cell>> M, Pos origen) 
+  {
+    vector<vector<int>> dist(M.size(),vector<int>(M[0].size(), INF));   //pones todos a distancia infinita
+    queue<pair<int,int>> Q;
+    vector<vector<pair<int,int>>> previo;
+
+    Q.push({origen.i, origen.j});
+    dist[origen.i][origen.j] = 0;
+
+    while (not Q.empty()) 
+    {
+      pair<int,int> p = Q.front(); 
+      Q.pop();
+      for (auto& d : dirs) {
+        int v_i = p.first + d.first;
+        int v_j = p.second + d.second;
+        if (bon_vei(M,v_i,v_j) and dist[v_i][v_j] == INF) 
+        {
+	        dist[v_i][v_j] = dist[p.first][p.second] + 1;
+          previo[v_i][v_j] = p;
+	        if (M[v_i][v_j].treasure) return;   //si encuentra un tesoro acaba == al destino/tesoro mas cerca
+	        Q.push({v_i,v_j});
+        }
+      }
+    }
+  }
+
+  void write(int v)   //Camino mas corto desde origen a v=destino=tesoro mas cerca
+  {
+    if (v == origen) cout << v;
+    else {
+        write(pare[v]);
+        cout << " " << v;
+    }
+  }
+
+
+  void camino_corto(Unit &u)
+  {
+    int n=rows(), m=cols();
+    vector<vector<Cell>> M(n,vector<Cell>(m));
+    for (int i = 2; i < n; ++i)
+    {
+      for (int j = 2; j < m; ++j)
+      {
+        M[i][j]=cell(i,j);
+      }
+    }
+
+    bfs_tresor(M,u.pos);
+
+  }
 
 
 
-  map<int,pair<bool,pair<Dir,Dir>>> en_muro;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+  //En cueva
+  void en_cueva(Unit &u)
+  {
+    bool moved=false;
+    for (int d = 0; d < 8 and not moved; ++d) 
+    {
+      Pos p1 = u.pos + Dir(d);
+      Cell c1 = cell(p1);
+      //Si hay tesoro
+      if(c1.treasure)
+      {
+        command(u.id,Dir(d));
+        moved=true;
+      }
+      //Si hay balrog escapar
+      else if(balrog_cerca(u))
+      {
+        //escapar
+
+      }
+      //Si hay enemigo con poca vida
+      else if(c1.id!= -1 and not unidad_mia(c1.id))
+      {
+        if(unit(c1.id).health < 20 ) 
+        {
+          command(u.id,Dir(d));
+          moved=true;
+        }
+      }
+      //Si hay cueva que no es mia
+      /*else if(c1.type==Cave and c1.owner != me())
+      {
+        command(u.id,Dir(d));
+        moved=true;
+      }*/
+    }
+    if(not moved)
+    {
+      //ir_tesoro(u);
+    }
+  }
+
+   
+
+  /**
+   * Play method, invoked once per each round.
+   */
+  virtual void play () 
+  {
+    //mover dwarves
+    for(unsigned int i=0; i<dwarves(me()).size(); ++i)
+    {
+      int id=dwarves(me())[i];
+      Unit u= unit(id);
+
+      //si esta en el exterior
+      if(cell(u.pos).type==Outside)
+      {
+        entrar_dentro(u);
+      }
+      else
+      {
+        en_cueva(u);      
+      }
+    }
+    //mover wizards
+    for(unsigned int i=0; i<wizards(me()).size(); ++i)
+    {
+      int id=wizards(me())[i];
+      Unit u= unit(id);
+
+      //si esta en el exterior
+      if(cell(u.pos).type==Outside)
+      {
+        entrar_dentro(u);
+      }
+      else
+      {
+        //ir al dwarv con menos vida mas cerca
+        curar_dwarv();
+      }
+    }
+  }
+};
+
+
+/**
+ * Do not modify the following line.
+ */
+RegisterPlayer(PLAYER_NAME);
+
+/*
+//Duende
+1. si hay enemigo cerca atacar
+2. orcos:
+2. trolls:
+4. balrog: mirar si esta a 2 casillas 
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+ map<int,pair<bool,pair<Dir,Dir>>> en_muro;
   map<int,bool> esquivando_muro;
   //Nuevo metodo esquivar
   void esquivar_muro(Unit &u)
@@ -255,96 +480,6 @@ struct PLAYER_NAME : public Player {
 
     }
   }
-
-
-
-
-
-
-  //En cueva
-  void en_cueva(Unit &u)
-  {
-    bool moved=false;
-    for (int d = 0; d < 8 and not moved; ++d) 
-    {
-      Pos p1 = u.pos + Dir(d);
-      Cell c1 = cell(p1);
-      //Si hay tesoro
-      if(c1.treasure)
-      {
-        command(u.id,Dir(d));
-        moved=true;
-      }
-      //Si hay cueva que no es mia
-      if(c1.type==Cave and c1.owner != me())
-      {
-        command(u.id,Dir(d));
-        moved=true;
-      }
-      //Si hay enemigo con poca vida
-      if(c1.id)
-      {
-        if
-
-      }
-    }
-  }
-
-   
-
-  /**
-   * Play method, invoked once per each round.
-   */
-  virtual void play () 
-  {
-    //mover dwarves
-    for(uint i=0; i<dwarves(me()).size(); ++i)
-    {
-      int id=dwarves(me())[i];
-      Unit u= unit(id);
-
-      //si esta en el exterior
-      if(cell(u.pos).type==Outside)
-      {
-        entrar_dentro(u);
-      }
-      else
-      {
-        en_cueva(u);
-      }
-
-
-    }
-
-    //mover wizards
-    for(uint i=0; i<wizards(me()).size(); ++i)
-    {
-      int id=wizards(me())[i];
-      Unit u= unit(id);
-
-      //si esta en el exterior
-      if(cell(u.pos).type==Outside)
-      {
-        entrar_dentro(u);
-      }
-
-    }
-  }
-
-};
-
-
-/**
- * Do not modify the following line.
- */
-RegisterPlayer(PLAYER_NAME);
-
-/*
-//Duende
-1. si hay enemigo cerca atacar
-2. orcos:
-2. trolls:
-4. balrog: mirar si esta a 2 casillas 
 
 
 
